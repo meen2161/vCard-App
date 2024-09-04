76ab93891a780a6047d888f4905a6713f9759927
meen2161 <meen2161@gmail.com>
2024-09-04 15:31:45 +0700

    📦Add : QR code package

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import QRCode from 'react-native-qrcode-svg';
+
+const Stack = createStackNavigator();
+
 export default function App() {
+  let name = "Kongrak;Thanawat;"
+
+  let data ="BEGIN:VCARD\n";
+  data += "VERSION:4.0\n";
+  data += "TITLE:\n";
+  data += "ORG:\n";
+  data += "N:" + name + "\n"
+  data += "TEL;TYPE=CELL:+66 0642649867\n";
+  data += "EMAIL:\n";
+  data += "URL:\n";
+  data += "URL:\n";
+  data += "URL:\n";
+  data += "END:VCARD";
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your Hello!</Text>
       <StatusBar style="auto" />
+      <View>
+        <QRCode value={data}/>
+      </View>
     </View>
   );
 }
